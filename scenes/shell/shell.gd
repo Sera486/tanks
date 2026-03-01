559c5d97acaab1de05cf20480a7e64dc3678ad42
unknown <iLorenc326@gmail.com>
2026-03-01 20:59:14 +0200

    Initial logic for shell movement and hit

--- scenes/shell/shell.gd
+++ scenes/shell/shell.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+const SPEED = 700.0
+
+var destination := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,9 +11,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction := position.direction_to(destination)
+	position += direction * delta * SPEED
+	rotation = destination.angle_to_point(position)
 
+func set_destination(coords: Vector2):
+	destination = coords
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("hit"):
-		body.hit(self)
+	if body.has_method("get_shot"):
+		body.get_shot(self)
