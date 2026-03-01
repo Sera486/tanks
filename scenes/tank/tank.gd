239d23fa197b711f6fbe0abdd2c4407561e187f3
unknown <iLorenc326@gmail.com>
2026-03-01 21:00:31 +0200

    Rotate tank turret and spawn shell

--- scenes/tank/tank.gd
+++ scenes/tank/tank.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
+@onready var turret = $TurretSprite2D2
+@onready var shell_spawn = $TurretSprite2D2/Marker2D
 
-const SPEED = 300.0
+@export var shell_scene: PackedScene 
+
+const SPEED = 200.0
 const ROTATION_SPEED = 10.0
 
 func _ready() -> void:
@@ -13,5 +17,21 @@ func _physics_process(delta: float) -> void:
 
 	rotation += rotation_direction * delta * ROTATION_SPEED	
 	velocity = transform.y * movement_direction * SPEED * -1
-
+	
+	turret.look_at(get_global_mouse_position())
+	
 	move_and_slide()
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		shoot(get_global_mouse_position())
+	
+func shoot(coords: Vector2):
+	var shell := shell_scene.instantiate(PackedScene.GEN_EDIT_STATE_DISABLED)
+	#shell.position = shell_spawn.position
+	shell.set_destination(coords)
+	shell_spawn.add_child(shell)
+
+func get_shot(bullet: Node2D):
+	#bullet.queue_free()
+	print("you've been hit")
